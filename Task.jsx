61a36c6310c5366c142e5d70e861c3afb2a0fba9
𝐑 𝐀 𝐇 𝐄 𝐄 𝐒 <114414128+rahhees@users.noreply.github.com>
2025-08-07 23:36:07 +0530

    Update Task.jsx

--- Task.jsx
+++ Task.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 
 function Task() {
     const [color,setColor]=useState("#FFFFFF")
+    const handleColor = ()=>{
+        setColor(prevColor=>prevColor==="#FFFFFF"?"#2e2a20":"#FFFFFF" )
+    }
+    
   return (
     <div style={{backgroundColor:color,height:'100vh',padding:'20px'}}>
         <h3>SELECT COLOR</h3>
-        <input type='color' value={color} onChange={(e)=>setColor(e.target.value)}/>
+  
+        <button onClick={handleColor}>TOGGLE BUTTON</button>
     </div>
   )
 }
